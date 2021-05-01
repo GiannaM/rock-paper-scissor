0d73bc988953e8c9a481a4121138e518ab3f1776
Gianna Pena <giannimpe1@yahoo.com>
2021-04-30 22:54:29 -0400

    if userHand selected add components Scoreboard, Contest and  hans

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import { useSelector } from 'react-redux';
 import './App.css';
+import Contest from './components/Contest';
+import Hands from './components/Hands';
+import ScoreBoard from "./components/ScoreBoard"
+
 
 function App() {
+
+  const userHand = useSelector(state => state.game.userHand)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScoreBoard/>
+        {userHand? <Contest/>:<Hands/>}
+
     </div>
   );
 }
