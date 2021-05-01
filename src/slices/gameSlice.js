9aee85a6320687fddb5302676f9c850a65e5ddee
Gianna Pena <giannimpe1@yahoo.com>
2021-04-30 22:52:20 -0400

    Create  gameSlice

---
+++ src/slices/gameSlice.js
@@ -0,0 +1,30 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+//tutorial sheet from Redux
+const initialState = {
+          userHand:"",
+          computerHand:""
+}
+
+const gameSlice = createSlice({
+          name: "game",
+          initialState:{
+            userHand: "",
+            computerHand: "",
+          },
+          reducers: {
+                    setUserHand: (state, action) => {
+                              state.userHand = action.payload;
+                    },
+
+                    setComputerHand: (state, action) => {
+                    state.computerHand = action.payload
+                    }
+          }
+
+
+});
+
+export const {setUserHand, setComputerHand,name} = gameSlice.actions;
+
+export default gameSlice.reducer;
